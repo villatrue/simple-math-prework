96430d8b08b9485403a065286d0874d8b52fb6e1
Sean Carter <scarter0313@gmail.com>
2019-02-27 00:13:29 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -15,9 +15,11 @@ def multiplication(num1, num2)
 end
 
 def modulo(num1, num2)
+  return 34%5
 end
 
 def square_root(num)
+  return math.sqrt(81)
 end
 
 def order_of_operation(num1, num2, num3, num4)
